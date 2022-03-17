42cd0de5ab46cb65d3dd95bb4cf8ff820defab5a
Jared Hernandez <herjaredsinai@gmail.com>
2022-03-17 19:28:31 -0400

    Create math functions

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function add(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+function multiply(firstNumber,secondNumber){
+    return firstNumber * secondNumber;
+}
+function divide(firstNumber,secondNumber){
+    return firstNumber / secondNumber;
+}
+
+console.log(add(1,2));
+console.log(subtract(1,2));
+console.log(multiply(1,2));
+console.log(divide(1,2));
